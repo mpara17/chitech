35d0bf00e0cf9964bc55eb387d0d0062d1d3cf54
mpara17 <monicapara2019@gmail.com>
2025-06-06 18:00:47 -0500

    added analytics and about page

--- app.py
+++ app.py
@@ -6,13 +6,9 @@ app = Flask(__name__)
 def index():
     return render_template('index.html')
 
-@app.route('/internship')
-def internship():
-    return render_template('internship.html')
-
-@app.route('/jobs')
-def jobs():
-    return render_template('jobs.html')
+@app.route('/about')
+def about():
+    return render_template('about.html')
 
 if __name__ == '__main__':
     app.run(port=8000,debug=True) 
